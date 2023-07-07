4394f486ddd6af21446850d9607a58aa19e1c3a2
Aaron Wu <12989649+Aaron-Wu1@users.noreply.github.com>
2023-07-07 13:20:52 -0700

    cleaned up code and added comments

--- app.py
+++ app.py
@@ -5,22 +5,19 @@ import pandas as pd
 app = Flask(__name__)
 
 
-# @app.route("/rec", methods=["GET"])
-# def rec_recipe():
-#     # run model
-#     return "result", 200
-
-
 @app.route("/rec", methods=["POST"])
 def rec_recipe():
+    """recommends recipe based on ingredients list.
+
+    Query parameters:
+        - onlyi, checks if the user wants the recipe recommendation to only have their inputed ingredients
+    """
     res = []
     only_ingr = request.args.get("onlyi", type=bool)
     ingredients_input = request.json["ingredients"]
-    # ingredients = ingredients["ingredients"]
     if only_ingr:
         df = pd.read_csv("input/df_parsed.csv")
         pattern = "(?:" + " ".join([f"(?i){i}" for i in ingredients_input])
         print(type(df["ingredients_parsed"].str.lower()))
-        # print(df[df["ingredients"].str.contains(pattern)])
         res = df[df["ingredients"].str.contains(pattern)]
     return res.to_json(), 200
