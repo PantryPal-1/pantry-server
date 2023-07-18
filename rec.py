e1cb6ce081576650949c86282a65e9d031d0420d
Aaron Wu <12989649+aaron-wu1@users.noreply.github.com>
2023-07-17 18:58:42 -0700

    fixed rec datatable format (#9)
    * added ?n=int for recommending n recipes
    
    * added models with new dataset
    
    * added readme
    
    * fixed json post req

--- rec.py
+++ rec.py
@@ -1,6 +1,7 @@
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 
+
 class cosine_rec:
     def __init__(self, features, data) -> None:
         self.features = features
@@ -9,11 +10,15 @@ class cosine_rec:
     def predict(self, input, n):
         cosine_sim = cosine_similarity(input, self.features).flatten()
         scores = list(cosine_sim)
-        top_results = sorted(range(len(scores)), key=lambda i: scores[i], reverse=True)[:n]
-        recs = pd.DataFrame(columns=["recipe", "ingredients", "url", "score"])
+        top_results = sorted(range(len(scores)), key=lambda i: scores[i], reverse=True)[
+            :n
+        ]
+        recs = pd.DataFrame(
+            columns=["recipe_name", "ingredients", "recipe_urls", "score"]
+        )
         for i, ind in enumerate(top_results):
-            recs.at[i + 1, "recipe"] = self.data["recipe_name"][ind]
+            recs.at[i + 1, "recipe_name"] = self.data["recipe_name"][ind]
             recs.at[i + 1, "ingredients"] = self.data["ingredients"][ind]
-            recs.at[i + 1, "url"] = self.data["recipe_urls"][ind]
+            recs.at[i + 1, "recipe_urls"] = self.data["recipe_urls"][ind]
             recs.at[i + 1, "score"] = f"{scores[ind]}"
         return recs
